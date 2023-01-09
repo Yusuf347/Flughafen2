e88b581132fdb205bb93159d08c5813555e3d8cd
Yusuf347 <yusuf.cheik.mostafa@gymhum.de>
2023-01-09 23:18:54 +0100

    init

---
+++ Flughafen 2/src/App.java
@@ -0,0 +1,5 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, Informatik ist schwer, aber ich schafft das inshallah!");
+    }
+}
